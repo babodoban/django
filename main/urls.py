4505cb64d5229ece41540dd2a05ded64de7ae6e5
Sean <babodoban@gmail.com>
2019-02-23 02:14:06 +0900

    2nd week challenge 2
    앱이 여러개일 때 main의 urls.py에서 모든 앱의 ulr을 한 번에 로드할 수 있게 하는 방법으로 보이는 것을 해보려고 했는데, 실패...

--- main/urls.py
+++ main/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from facebook.views import play
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("facebook.urls")),
+    path('play/', play)
 ]
